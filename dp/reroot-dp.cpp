ccb64943e5c00291e6c2ee9aedd3f5d9816fbea5
aganoyot <aganoyot@gmail.com>
2020-07-06 19:13:15 +0900

    a

---
+++ dp/reroot-dp.cpp
@@ -0,0 +1,121 @@
+//
+// Reroot
+//
+// verified
+// [GRL_5_A < Problems | Aizu Online Judge](https://onlinejudge.u-aizu.ac.jp/problems/GRL_5_A)
+//
+// 参考
+// https://algo-logic.info/tree-dp/
+//
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) int(x.size())
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+
+const ll INF = 1LL << 60;
+
+struct Rerooting {
+    /* TODO: start 問題ごとに書き換え */
+    struct DP {  // DP の型
+        int dp;
+        DP(int dp_) : dp(dp_) {}
+    };
+
+    // 単位元はしっかり定義する(末端でもadd_rootされるので注意)
+    const DP identity = DP(0);
+
+    // d:辺eに対応する部分木のdpの値  cost:eのコスト
+    function<DP(DP, DP, long long)> merge = [](DP dp_cum, DP d, long long cost) -> DP {
+        return DP(max(dp_cum.dp, d.dp + (int)cost));
+    };
+
+    // まとめたDPを用いて、新たな部分木のDPを計算する
+    function<DP(DP)> add_root = [](DP d) -> DP {
+        return DP(d.dp);
+    };
+    /* TODO: end 問題ごとに書き換え */
+
+    // グラフの定義
+    struct Edge {
+        int from;
+        int to;
+        long long cost;
+    };
+    using Graph = vector<vector<Edge>>;
+
+    // dp[v][i]: vから出るi番目の有向辺に対応する部分木のDP
+    vector<vector<DP>> dp;
+
+    // ans[v]: 頂点vを根とする木の答え
+    vector<DP> ans;
+    Graph G;
+
+    Rerooting(int N) : G(N) {
+        dp.resize(N);
+        ans.assign(N, identity);
+    }
+
+    void add_edge(int a, int b, long long c = 1) {
+        G[a].push_back({a, b, c});
+    }
+    void build() {
+        dfs(0);            // 普通に木DP
+        bfs(0, identity);  // 残りの部分木に対応するDPを計算
+    }
+
+    DP dfs(int v, int p = -1) {  // 頂点v, 親p
+        DP dp_cum = identity;
+        int deg = G[v].size();
+        dp[v] = vector<DP>(deg, identity);
+        for (int i = 0; i < deg; i++) {
+            int u = G[v][i].to;
+            if (u == p) continue;
+            dp[v][i] = dfs(u, v);
+            dp_cum = merge(dp_cum, dp[v][i], G[v][i].cost);
+        }
+        return add_root(dp_cum);
+    }
+    void bfs(int v, const DP &dp_p, int p = -1) {
+        int deg = G[v].size();
+        for (int i = 0; i < deg; i++) {  // 前のbfsで計算した有向辺に対応する部分木のDPを保存
+            if (G[v][i].to == p) dp[v][i] = dp_p;
+        }
+
+        vector<DP> dp_l(deg + 1, identity), dp_r(deg + 1, identity);  // 累積的なDP
+        for (int i = 0; i < deg; i++) {
+            dp_l[i + 1] = merge(dp_l[i], dp[v][i], G[v][i].cost);
+        }
+        for (int i = deg - 1; i >= 0; i--) {
+            dp_r[i] = merge(dp_r[i + 1], dp[v][i], G[v][i].cost);
+        }
+        ans[v] = add_root(dp_l[deg]);
+        for (int i = 0; i < deg; i++) {
+            int u = G[v][i].to;
+            if (u == p) continue;
+
+            // 累積同士のmergeなので、edgeは適当に
+            bfs(u, add_root(merge(dp_l[i], dp_r[i + 1], 0)), v);
+        }
+    }
+};
+
+ll n;
+int main() {
+    cin >> n;
+    Rerooting reroot(n);
+    rep(i, n - 1) {
+        ll s, t, w;
+        cin >> s >> t >> w;
+        reroot.add_edge(s, t, w);
+        reroot.add_edge(t, s, w);
+    }
+    reroot.build();
+
+    ll ans = 0;
+    rep(i, n) {
+        ans = max(ans, (ll)reroot.ans[i].dp);
+    }
+    cout << ans << endl;
+}
