8a9171764b92930090702e0b0a89f773ec32de1c
aganoyot <aganoyot@gmail.com>
2020-07-08 22:15:36 +0900

    a

---
+++ think-estimate/disassemble.cpp
@@ -0,0 +1,96 @@
+//
+// 計算式を分解して考える
+//
+// f(X) = max X − min X を下記のように分解する
+// 1. + max X
+// 2. - min X
+//
+// verified
+// [E - Max-Min Sums](https://atcoder.jp/contests/abc151/tasks/abc151_e)
+//
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) int(x.size())
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+
+const ll INF = 1LL << 60;
+
+const int mod = 1e9 + 7;
+
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+struct combination {
+  vector<mint> fact, ifact;
+  combination(int n):fact(n+1),ifact(n+1) {
+    assert(n < mod);
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) fact[i] = fact[i-1]*i;
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) ifact[i-1] = ifact[i]*i;
+  }
+  mint operator()(int n, int k) {
+    if (k < 0 || k > n) return 0;
+    return fact[n]*ifact[k]*ifact[n-k];
+  }
+};
+
+ll N, K;
+vector<ll> A;
+
+int main() {
+    cin >> N >> K;
+    A.resize(N);
+    rep(i, N) cin >> A[i];
+
+    sort(A.begin(), A.end());
+
+    combination comb(1e5 + 10);
+
+    // 最大と最小を分解する
+    mint ans = 0;
+
+    for (int m = 0; m < 2; m++) {
+        for (int i = 0; i < N; i++) {
+            mint now = comb(i, K - 1);
+            if (m == 1) now = -now;
+
+            ans += now * A[i];
+        }
+
+        reverse(A.begin(), A.end());
+    }
+
+    cout << ans << endl;
+}
