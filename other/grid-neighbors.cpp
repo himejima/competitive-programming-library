f42e9b5762431404ecd7e061de9201cea46c58f4
aganoyot <aganoyot@gmail.com>
2020-07-08 22:24:34 +0900

    a

---
+++ other/grid-neighbors.cpp
@@ -0,0 +1,24 @@
+// グリッドの 4近傍、8近傍
+//
+// template に入れておいてもいいかも
+//
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) int(x.size())
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+
+const ll INF = 1LL << 60;
+
+// 4 近傍
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+
+// 8 近傍
+const int dx8[8] = {1, 0, -1, 0, 1, -1, 1, -1};
+const int dy8[8] = {0, 1, 0, -1, 1, -1, -1, 1};
+
+int main() {
+}
