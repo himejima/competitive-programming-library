d8ee50abf1bb6e7eabd378d5439299c2e83001c3
aganoyot <aganoyot@gmail.com>
2020-07-09 22:19:37 +0900

    a

---
+++ think-estimate/deformation-of-formula.cpp
@@ -0,0 +1,39 @@
+//
+// 計算式を変形する
+// 同じ変数通しで、右辺と左辺に分ける
+//
+// verified
+// [E - This Message Will Self-Destruct in 5s](https://atcoder.jp/contests/abc166/tasks/abc166_e)
+//
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define sz(x) int(x.size())
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+
+const ll INF = 1LL << 60;
+
+ll N;
+vector<ll> A;
+int main() {
+    cin >> N;
+    A.resize(N);
+    rep(i, N) cin >> A[i];
+
+    map<ll, ll> mp;
+    rep(j, N) {
+        mp[j + 1 - A[j]]++;
+    }
+
+    // for (auto iter = mp.begin(); iter != mp.end(); iter++) {
+    //     cout << iter->first << " " << iter->second << endl;
+    // }
+
+    ll ans = 0;
+    rep(i, N) {
+        ll target = (i + 1) + A[i];
+        ans += mp[target];
+    }
+    cout << ans << endl;
+}
